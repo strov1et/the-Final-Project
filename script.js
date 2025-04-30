4abbb2221e1d9c7f5dd32e871fa7fecda17a0224
mariam iremadze <mariamm.iremadze@gmail.com>
2025-04-29 23:41:37 -0400

    .

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ burger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
   burger.classList.toggle("open");
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  AOS.init({
+    duration: 1000,
+    once: true,
+    offset: 200,
+  });
+});
